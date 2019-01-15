5e439ff70afda59be6b3a72a2b256c8ba248b21c
Hapachino <erickwbrownfield@gmail.com>
2019-01-14 17:23:23 -0800

    completed all pages and routes

---
+++ src/components/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => (
+  <div>
+    <h1 className="center">About Movie Quotes</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A quos, reiciendis modi porro expedita reprehenderit repudiandae explicabo obcaecati, sint cumque vel dolore aspernatur? Odit, sunt fugiat deserunt ea at corporis!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi doloribus nemo ut sapiente deserunt? Ex illum a blanditiis veniam nobis aspernatur, deserunt cumque ad omnis?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore commodi ducimus laborum expedita maiores cumque!</p>
+  </div>
+);
